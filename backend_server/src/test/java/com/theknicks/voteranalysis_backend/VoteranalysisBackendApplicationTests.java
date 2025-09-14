df38dc168c98cf3bea8a993d8aae101eb8555835
Jerry Zhu <jerry.zhu@stonybrook.edu>
2025-09-14 16:40:46 -0400

    Simple test...

--- backend_server/src/test/java/com/theknicks/voteranalysis_backend/VoteranalysisBackendApplicationTests.java
+++ backend_server/src/test/java/com/theknicks/voteranalysis_backend/VoteranalysisBackendApplicationTests.java
@@ -1,13 +1,25 @@
 package com.theknicks.voteranalysis_backend;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class VoteranalysisBackendApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void dummyTest() {
+		// Arrange
+	 	JdbcTemplate jdbcTemplate = Mockito.mock(JdbcTemplate.class);
+		DummyController _dummyController = new DummyController(jdbcTemplate);
+		
+		// Act
+		var result = _dummyController.handleTest1();
 
+		// Assert
+		assertNotNull(result);
+		assertEquals(result, "<h1>Hello</h1>");
+	}
 }
