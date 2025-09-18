d2f5538bebbbcd607249f0470efbe7d38d60c78e
jerzhu <143643544+jerzhu@users.noreply.github.com>
2025-09-17 21:12:24 -0400

    [Frontend] State View Transition P.O.C (#7)
    * Updated some ref stuff, also handling on click events.
    
    * Added react-router
    
    * removed unused useCallback
    
    * some react-router experimental stuff
    
    * React Router + Proof of Concept for State Info Transition
    
    There's... a lot of stuff in here, but it's based off the current
    figma design basically.
    
    Regardless, the information we display doesn't change, only how we display it
    so I'm okay kind of rapidly iterating, and we do have the react-router system working
    for it anyway.

--- frontend_client/src/App.tsx
+++ frontend_client/src/App.tsx
@@ -4,17 +4,18 @@ import 'leaflet/dist/leaflet.css';
 import { GeoJSON, MapContainer, TileLayer } from 'react-leaflet';
 import { getTestEndpoint } from './voterAPI.tsx';
 import { statesData } from './TestChoroplethData';
-import L, { type PathOptions, type StyleFunction } from 'leaflet';
-import React, { useEffect, useState } from 'react';
+import L, { FeatureGroup, type LeafletMouseEvent, type PathOptions, type StyleFunction } from 'leaflet';
+import React, { useEffect, useState, useRef } from 'react';
+import { Routes, Route, useParams, useLocation, useNavigate, Link } from 'react-router';
 import type { MapRef } from 'react-leaflet/MapContainer';
 
 interface GradientMap {
   [key: number]: string
 };
 
-interface MapLegendParameters {
-  leafletMap: MapRef,
-  gradientMap: GradientMap,
+// For the background overlay system
+interface RouterLocationState {
+  backgroundLocation?: Location;
 }
 
 const CHOROPLETH_COLOR_MAP: GradientMap  = {
@@ -65,6 +66,10 @@ function geoJsonFeatureColorStyle(gradientMap: GradientMap): StyleFunction {
   }
 }
 
+interface MapLegendParameters {
+  leafletMap: React.RefObject<MapRef>,
+  gradientMap: GradientMap,
+}
 function MapChoroplethLegend({ leafletMap , gradientMap } : MapLegendParameters) {
   /*
      NOTE(jerry):
@@ -73,7 +78,8 @@ function MapChoroplethLegend({ leafletMap , gradientMap } : MapLegendParameters)
   */
   useEffect(
     function() {
-      if (leafletMap) {
+      const mapState = leafletMap.current;
+      if (mapState) {
         const legend = new L.Control({position: 'bottomright'});
         legend.onAdd = function () {
           const div = L.DomUtil.create('div', 'info legend');
@@ -89,45 +95,184 @@ function MapChoroplethLegend({ leafletMap , gradientMap } : MapLegendParameters)
           return div;
         };
 
-        legend.addTo(leafletMap);
+        legend.addTo(mapState);
       }
     },
     [leafletMap, gradientMap])
   return null;
 }
+interface MainScreenMapParameters {
+  leafletMap : React.RefObject<MapRef>,
+  data : any, // The type is very complicated to annotate here.
+};
 
-function App() {
-  const [mapState, setMapState] = useState<MapRef>(null);
-  const [backendMessage, setBackendMessage] = useState("");
+/**
+ * This is the map used for the landing / splash page of the viewer.
+ * 
+ * Submaps will be defined as separate components.
+ */
+function MainScreenMap({ leafletMap, data } : MainScreenMapParameters) {
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  useEffect(() => {
-    getTestEndpoint()
-    .then(function (res) {
-      setBackendMessage(res.data);
-    })
-    .catch(function (err) {
-      console.error("Error connecting to backend: ", err)
+  function onFeatureClickHandler (event: LeafletMouseEvent) {
+    const target = event.target as FeatureGroup;
+    const featureData = target.feature as GeoJSON.Feature;
+
+    if (featureData.properties) {
+      navigate(
+        `/state/${featureData.properties.name}`,
+        {
+          state: { backgroundLocation: location }
+        });
+    }
+
+    // Zoom into map.
+    const mapState = leafletMap.current;
+    mapState?.fitBounds(target.getBounds());
+  }
+
+  function onEachFeatureHandler(_feature: GeoJSON.Feature, layer: L.Layer) {
+    layer.on({
+      click: onFeatureClickHandler
     });
-  }, []);
+  }
 
   return (
     <React.Fragment>
-      <h2>Backend says: {backendMessage}</h2>
+        <GeoJSON 
+        style={geoJsonFeatureColorStyle(CHOROPLETH_COLOR_MAP)}
+        data={data as GeoJSON.GeoJSON}
+        onEachFeature={onEachFeatureHandler}/>
+        <MapChoroplethLegend leafletMap={leafletMap} gradientMap={CHOROPLETH_COLOR_MAP}/>
+    </React.Fragment>
+  );
+}
+
+function LandingPage() {
+  const mapState = useRef<MapRef>(null);
+
+  return (
+    <React.Fragment>
       <MapContainer
         zoom={4}
         bounds={UNITED_STATES_BOUNDARIES}
         maxBounds={UNITED_STATES_BOUNDARIES}
         maxBoundsViscosity={1.0}
-        ref={setMapState}
+        ref={mapState}
         id="main-map">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON style={geoJsonFeatureColorStyle(CHOROPLETH_COLOR_MAP)} data={statesData as GeoJSON.GeoJSON}/>
-        <MapChoroplethLegend leafletMap={mapState} gradientMap={CHOROPLETH_COLOR_MAP}/>
+        <MainScreenMap
+          leafletMap={mapState}
+          data={statesData as GeoJSON.GeoJSON}/>
       </MapContainer>
     </React.Fragment>
+  );
+}
+
+function NotFoundPage() {
+  return (
+    <React.Fragment>
+      <h1>This page is invalid!</h1>
+      <p>Your data is in another castle!</p>
+    </React.Fragment>
+  );
+}
+
+function StateInformationPopup() {
+  const { name } = useParams();
+  return (
+    <div className="state-information-popup">
+      {/* These are the contents of the drop-down. */}
+      <div className="state-information-side-modal">
+        <ul className="state-information-side-modal-options">
+          <li>Provisional Ballots</li>
+          <li>Mail-in Ballots</li>
+          <li>Absentee Ballots</li>
+          <li>Voter Machine Information</li>
+          <li><button><Link to={"/"}>Back</Link></button></li>
+        </ul>
+      </div>
+      <div className="state-information-main-modal">
+        <h1>
+          {name}
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+
+Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function BackgroundBlurrer() {
+  return (
+    <div id="background-blurrer"/>
+  );
+}
+
+function App() {
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/state/:name/*" element={<LandingPage/>}/>
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
+      {/* TODO(jerry): This is wrong, should be state flag. */}
+      <Routes>
+        <Route path="/state/:name/*" element={<BackgroundBlurrer/>}/>
+        <Route path="*" element={<React.Fragment/>}/>
+      </Routes>
+      {/* NOTE(jerry): Needed in order to do the overlay effect that I think looks cool. */}
+      <Routes>
+        <Route path="state/:name/*" element={<StateInformationPopup/>}/>
+        <Route path="*" element={<React.Fragment/>}/>
+      </Routes> 
+   </React.Fragment>
   )
 }
 
