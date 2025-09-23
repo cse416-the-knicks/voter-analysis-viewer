4b7555674cf741da9a82ad3e331daebcae413b4a
Jerry Zhu <jerry.zhu@stonybrook.edu>
2025-09-22 23:34:14 -0400

    Front page face lift

--- frontend_client/src/components/FrontPage/index.tsx
+++ frontend_client/src/components/FrontPage/index.tsx
@@ -7,6 +7,47 @@ import type { FipsCode } from '../FullBoundedUSMap/';
 
 import styles from './FrontPage.module.css';
 
+import {
+  Button,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Box,
+} from '@mui/material';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+
+function FrontPageDrawer() {
+  const navigate = useNavigate();
+
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+	'& .MuiDrawer-paper': {
+	  width: '14em',
+	  margin: 0,
+	},
+      }}
+    >
+      <List dense>
+	<ListItem> <ListItemText primary={"COMPARE VOTING"}/> </ListItem>
+	<ListItem> <ListItemButton onClick={() => navigate("/compare/parties")}> <ListItemText primary={"BIPARTISAN EARLY VOTING"}/> </ListItemButton> </ListItem>
+	<ListItem> <ListItemButton onClick={() => navigate("/compare/optvote")}> <ListItemText primary={"OPT-IN, OPT-OUT VOTING"}/> </ListItemButton> </ListItem>
+	<Divider/>
+	<ListItem> <ListItemText primary={"DISPLAY"}/> </ListItem>
+	<ListItem> <ListItemButton onClick={() => navigate("/display/voting-machine-age")}> <ListItemText primary={"VOTING EQUIPMENT AGE"}/> </ListItemButton> </ListItem>
+	<ListItem> <ListItemButton onClick={() => navigate("/display/voting-machine-summary")}> <ListItemText primary={"VOTING EQUIPMENT 2024 SUMMARY"}/> </ListItemButton> </ListItem>
+      </List>
+      <Button variant='contained' color='secondary'>
+	<HighlightOffIcon/> Reset to Default
+      </Button>
+    </Drawer>
+  );
+}
+
 /**
  * This is the map used for the landing / splash page of the viewer.
  * 
@@ -17,17 +58,19 @@ function FrontPage() {
   const navigate = useNavigate();
 
   const onStateClick = (fipsCode: FipsCode) => {
-    window.alert(fipsCode);
-    navigate(`state/${fipsCode}`);
+    navigate(`/state/${fipsCode}`);
   }
 
   return (
     <React.Fragment>
+      <Box>
+	<FrontPageDrawer/>
       <FullBoundedUSMap
 	mapRef={mapState}
 	id={styles.mainMap}
 	onStateClick={onStateClick}>
       </FullBoundedUSMap>
+      </Box>
     </React.Fragment>
   );
 }
