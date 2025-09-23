5057de37bd13fdd68bbb482de2166fb8be27c44e
Jerry Zhu <jerry.zhu@stonybrook.edu>
2025-09-22 22:57:37 -0400

    Improved sub-component behavior...

--- frontend_client/src/components/StateInformationView/index.tsx
+++ frontend_client/src/components/StateInformationView/index.tsx
@@ -1,7 +1,8 @@
+import type { ReactNode } from 'react';
+import type { GridColDef } from '@mui/x-data-grid';
+
 import { useNavigate, useParams } from 'react-router';
-import styles from './StateInformationView.module.css';
-import StateMap from '../StateMap';
-import { FIPS_TO_STATES_MAP } from '../FullBoundedUSMap/boundaryData';
+import { DataGrid } from '@mui/x-data-grid';
 import InboxIcon from '@mui/icons-material/Inbox';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
@@ -10,9 +11,29 @@ import PersonIcon from '@mui/icons-material/Person';
 import PersonOffIcon from '@mui/icons-material/PersonOff';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import ScannerIcon from '@mui/icons-material/Scanner';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import Stack from '@mui/material/Stack';
-import type { GridColDef } from '@mui/x-data-grid';
-import { DataGrid } from '@mui/x-data-grid';
+
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Paper,
+  Typography
+} from '@mui/material';
+
+import { useState } from 'react';
+
+import styles from './StateInformationView.module.css';
+import { FIPS_TO_STATES_MAP } from '../FullBoundedUSMap/boundaryData';
+import StateMap from '../StateMap';
 
 const columns: GridColDef<(typeof rows)[number]>[] = [
   { field: 'id', headerName: 'ID', width: 90 },
@@ -57,41 +78,120 @@ const rows = [
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+interface StateInformationViewDrawerItem {
+  id: number;
+  iconComponent?: ReactNode;
+  textContent: string;
+};
+interface StateInformationViewDrawerSection {
+  title: string;
+  iconComponent?: ReactNode;
+  items: StateInformationViewDrawerItem[];
+};
 
-import {
-  Box,
-  Button,
-  Divider,
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Paper,
-  Typography
-} from '@mui/material';
+interface StateInformationViewDrawerProperties {
+  stateHook: [number, (arg0: number) => void];
+  sections: StateInformationViewDrawerSection[];
+};
 
-function StateInformationView() {
-  const { fipsCode } = useParams();
-  console.log(fipsCode);
+interface StateInformationViewDrawerListItemProperties {
+  stateHook: [number, (arg0: number) => void];
+  item: StateInformationViewDrawerItem;
+};
+
+function StateInformationViewDrawerListItem(
+  {
+    item,
+    stateHook,
+  }: StateInformationViewDrawerListItemProperties) {
+  const [stateValue, setStateValue] = stateHook;
+
+  return (
+    <ListItem>
+	<ListItemButton
+	  key={item.id}
+	  onClick={() => setStateValue(item.id)}
+	  selected={stateValue == item.id}>
+	    {((item.iconComponent) && <ListItemIcon>{item.iconComponent}</ListItemIcon>)}
+	    <ListItemText primary={item.textContent}/>
+	</ListItemButton>
+    </ListItem>
+  );
+}
+
+function StateInformationViewDrawer(
+  {
+    sections,
+    stateHook,
+  }: StateInformationViewDrawerProperties) {
   const navigate = useNavigate();
-  const card = (
-    <>
+  const sectionComponents = sections.map(
+    (section) => (
+      <>
+	<ListItem>
+	    {((section.iconComponent) && <ListItemIcon>{section.iconComponent}</ListItemIcon>)}
+	    <ListItemText primary={section.title}/>
+	</ListItem>
+	{
+	  section.items.map(item => <StateInformationViewDrawerListItem stateHook={stateHook} item={item}/>)
+	}
+      </>
+    )
+  );
+
+  const finalComponentsWithDividers = [];
+  for (let i = 0; i < sectionComponents.length; ++i) {
+    finalComponentsWithDividers.push(sectionComponents[i]);
+    if (i+1 >= sectionComponents.length) {
+      continue;
+    } else {
+      finalComponentsWithDividers.push(<Divider/>);
+    }
+  }
+
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+	'& .MuiDrawer-paper': {
+	  width: '14em',
+	  height: 'auto',
+	  margin: 2,
+	},
+      }}
+    >
+    <EAVsStateCard/>
+    <List dense>
+      {finalComponentsWithDividers}
+    </List>
+    <Button onClick={() => navigate("/")} variant='contained' color='secondary'>
+      <HighlightOffIcon/> Exit State Display
+    </Button>
+    </Drawer>
+  );
+}
+
+function EAVsStateCard() {
+  return (
+    <Card sx={{m: 2}} variant="outlined">
       <CardContent>
 	<Typography variant="h6" component="div">
-  EAVS-Only State
+	  EAVS-Only State
 	</Typography>
 	<Typography sx={{ color: 'text.secondary', m:0, fontSize: 12 }}>
 	  This is not a detail state, so information will be limited compared to select states.
 	</Typography>
       </CardContent>
-    </>
+    </Card>
   );
-  const detailCard = (
-    <>
+}
+
+// TODO(jerry):
+// Should accept a type parameter, but
+// for now I guess that's fine to not have it.
+function DetailStateCard() {
+  return (
+    <Card sx={{m: 2}} variant="outlined">
       <CardContent>
 	<Typography variant="h6" component="div">
 	  Voter Registation State
@@ -100,64 +200,41 @@ function StateInformationView() {
 	  This is a selected detail state for voter registration data, you can also view voter records for this state.
 	</Typography>
       </CardContent>
-    </>
+    </Card>
   );
+}
 
+function StateInformationView() {
+  const { fipsCode } = useParams();
+  const activeDataStateHook = useState(0);
+  const [activeDataSelection, setActiveDataSelection] = activeDataStateHook;
 
+  const dropDownSections = [
+    {
+      title: "Ballot Data",
+      iconComponent: <BallotIcon/>,
+      items: [
+	{ id: 0, iconComponent: <InboxIcon/>, textContent: "Provisional Ballots" },
+	{ id: 1, iconComponent: <PersonIcon/>, textContent: "Active Voters" },
+	{ id: 2, iconComponent: <DeleteForeverIcon/>, textContent: "Pollbook Deletions" },
+	{ id: 3, iconComponent: <PersonOffIcon/>, textContent: "Mail Ballot Rejections" },
+      ],
+    },
+    {
+      title: "Voting Equipment",
+      items: [
+	{ id: 4, iconComponent: <ScannerIcon/>, textContent: "By Type" },
+	{ id: 5, iconComponent: <AccessTimeIcon/>, textContent: "By Age" },
+      ],
+    }
+  ];
 
   return (
     <div className={styles.stateInformationPopup}>
       <Box>
-	<Drawer
-	  variant="permanent"
-	  sx={{
-	    '& .MuiDrawer-paper': { width: '14em', height: 'auto', margin: 2 },
-	  }}>
-	  <Card sx={{m: 2}} variant="outlined">{detailCard}</Card>
-	  <List dense>
-	  <ListItem>
-  <ListItemIcon><BallotIcon/></ListItemIcon>
-  <ListItemText primary={"Ballot Data"}/> </ListItem>
-	  <ListItem>
-	    <ListItemButton>
-  <ListItemIcon><InboxIcon/></ListItemIcon>
-  <ListItemText primary={"Provisional Ballots"}/>
-	    </ListItemButton>
-	  </ListItem>
-	  <ListItem>
-	    <ListItemButton>
-  <ListItemIcon><PersonIcon/></ListItemIcon>
-  <ListItemText primary={"Active Voters"}/>
-	    </ListItemButton>
-	  </ListItem>
-	  <ListItem>
-	    <ListItemButton>
-  <ListItemIcon><DeleteForeverIcon/></ListItemIcon>
-  <ListItemText primary={"Pollbook Deletions"}/>
-	    </ListItemButton>
-	  </ListItem>
-	  <ListItem>
-	    <ListItemButton>
-  <ListItemIcon><PersonOffIcon/></ListItemIcon>
-  <ListItemText primary={"Mail Ballot Rejections"}/>
-	    </ListItemButton>
-	  </ListItem>
-	  <Divider/>
-	  <ListItem> <ListItemText primary={"Voting Equipment"}/> </ListItem>
-	  <ListItem>
-	    <ListItemButton>
-  <ListItemIcon><ScannerIcon/></ListItemIcon>
-  <ListItemText primary={"By Type"}/> </ListItemButton>
-	  </ListItem>
-	  <ListItem>
-	    <ListItemButton>
-  <ListItemIcon><AccessTimeIcon/></ListItemIcon>
-  <ListItemText primary={"By Age"}/> </ListItemButton>
-	  </ListItem>
-	  </List>
-	  <Button onClick={() => navigate("/")} variant='contained' color='secondary'>
-  <HighlightOffIcon/> Exit State Display</Button>
-	</Drawer>
+	<StateInformationViewDrawer
+	  stateHook={activeDataStateHook}
+	  sections={dropDownSections}/>
       </Box>
 
       <Stack direction="column">
