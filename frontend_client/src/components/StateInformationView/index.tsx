62def696959363c9c3d45d3735cf414f0809f3a9
jerzhu <143643544+jerzhu@users.noreply.github.com>
2025-09-23 17:34:20 -0400

    Revert "Basic data visualizations through D3"

--- frontend_client/src/components/StateInformationView/index.tsx
+++ frontend_client/src/components/StateInformationView/index.tsx
@@ -2,9 +2,6 @@ import { Link, useParams } from 'react-router';
 import styles from './StateInformationView.module.css';
 import StateMap from '../StateMap';
 import { FIPS_TO_STATES_MAP } from '../FullBoundedUSMap/boundaryData';
-import { mockData } from '../DataDisplays/DisplayData';
-import DataTable from '../DataDisplays/DataTable';
-import BubbleChart from '../DataDisplays/BubbleChart';
 
 function StateInformationView() {
   const { fipsCode } = useParams();
@@ -26,12 +23,9 @@ function StateInformationView() {
           {FIPS_TO_STATES_MAP[fipsCode!]}
         </h1>
 	<StateMap fipsCode={fipsCode}/>
-        <div>
-          {/* Testing displays of data charts/tables */}
-          <h4>Data Dashboard</h4>
-            <BubbleChart data={mockData} width={800} height={350}/>
-            <DataTable data={mockData}/>
-        </div>
+        <p>
+	  Place holder layout. Fix me!
+        </p>
       </div>
     </div>
   );
