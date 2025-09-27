6dd7d4e6aab13087e6fe3d0f9f2d5192ded8e1ef
{gqgil} <{gabrielben.gil@stonybrook.edu}>
2025-09-26 23:32:41 -0400

    Main script changed to try to send most eavs_data to the sql table

--- data_common/scripts/load_2024_eavs_data.py
+++ data_common/scripts/load_2024_eavs_data.py
@@ -21,8 +21,14 @@ engine = create_engine(
 data_path = "../raw/2024_EAVS_for_Public_Release_V1_xlsx.xlsx"
 df = pd.read_excel(data_path, dtype={"FIPSCode": str})
 
-# Columns to pull from the spreadsheet (adjust to match actual headers)
-cols = ["FIPSCode","E1a","E2a","E2b","E2c","E2d","E2e","E2f","E2g","E2h","E2i","E2j","E2k","E2l"]
+# Columns to pull from the spreadsheet
+cols = ["FIPSCode",
+        "A1a","A1b","A1c",
+        "A12a","A12b","A12c","A12d","A12e","A12f","A12g","A12h","A12i","A12j","A12k",
+        "C8a","C3a",
+        "E1a","E2a","E2b","E2c","E2d","E2e","E2f","E2g","E2h","E2i","E2j","E2k","E2l",
+        "C9b","C9c","C9d","C9e","C9f","C9g","C9h","C9i","C9j","C9k","C9l","C9m","C9n","C9o","C9p","C9q",
+        "C9r","C9s","C9t"]
 df = df[cols]
 
 # Filtering out the UOCAVA Maine row
@@ -44,28 +50,66 @@ def to_int(val):
 for c in cols[1:]:
     df[c] = df[c].apply(to_int)
 
-# Compute prov_other as E2j + E2k + E2l (skip NaN)
+# Compute removed_other as A12i + A12j + A12k (skipping NaN)
+df["removed_other"] = df[["A12i","A12j","A12k"]].sum(axis=1, skipna=True, min_count=1)
+
+# Compute prov_other as E2j + E2k + E2l (skipping NaN)
 df["prov_other"] = df[["E2j","E2k","E2l"]].sum(axis=1, skipna=True, min_count=1)
+
+# Compute mail_reject_other as C9r + C9s + C9t (skipping NaN)
+df["mail_reject_other"] = df[["C9r","C9s","C9t"]].sum(axis=1, skipna=True, min_count=1)
 
 df["year"] = 2024
 
-# Dropping the unused columns E2j, E2k, E2l before writing
-df = df.drop(columns=["E2j","E2k","E2l"])
+# Dropping the unused other columns before writing
+df = df.drop(columns=["A12i","A12j","A12k","E2j","E2k","E2l","C9r","C9s","C9t"])
 
-# Mapping each E2* code to the actual schema column names
+# Mapping each code to the actual schema column names
 rename_map = {
     "FIPSCode": "region_id",
+    "A1a": "total_registered",
+    "A1b": "active_registered",
+    "A1c": "inactive_registered",
+    "A12a" : "total_removed",
+    "A12b" : "removed_moved",
+    "A12c" : "removed_deceased",
+    "A12d" : "removed_felony",
+    "A12e" : "removed_failed_confirm",
+    "A12f" : "removed_incompetent",
+    "A12g" : "removed_requested",
+    "A12h" : "removed_duplicate",
+    "removed_other" : "removed_other",
+    "C8a" : "total_ballots_cast",
+    "C3a" : "ballots_dropbox",
     "E1a": "prov_cast",
     "E2a": "prov_reason_not_in_roll",
     "E2b": "prov_reason_no_id",
-    "E2c": "prov_reason_not_eligibe_official",
+    "E2c": "prov_reason_not_eligible_official",
     "E2d": "prov_reason_challenged",
     "E2e": "prov_reason_wrong_precinct",
     "E2f": "prov_reason_name_address",
     "E2g": "prov_reason_mail_ballot_unsurrendered",
     "E2h": "prov_reason_hours_extended",
     "E2i": "prov_reason_same_day_reg",
-    "prov_other": "prov_other"
+    "prov_other": "prov_other",
+    "C9a" : "",
+    "C9b" : "",
+    "C9c" : "",
+    "C9d" : "",
+    "C9e" : "",
+    "C9f" : "",
+    "C9g" : "",
+    "C9h" : "",
+    "C9i" : "",
+    "C9j" : "",
+    "C9k" : "",
+    "C9l" : "",
+    "C9m" : "",
+    "C9n" : "",
+    "C9o" : "",
+    "C9p" : "",
+    "C9q" : "",
+    "mail_reject_other" : "mail_reject_other"
 }
 df = df.rename(columns=rename_map)
 
@@ -73,7 +117,7 @@ print(df)
 
 # Inserting into db
 df.to_sql(
-    "eavs_data",            # Fill with name of table
+    "eavs_data",
     engine,
     schema="app",
     if_exists="append",
