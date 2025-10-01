164417311718570a7a3adee9cac94be85e014ee6
Jerry Zhu <jerry.zhu@stonybrook.edu>
2025-10-01 01:48:49 -0400

    less shit choropleth legend

--- frontend_client/src/components/StateInformationView/index.tsx
+++ frontend_client/src/components/StateInformationView/index.tsx
@@ -356,6 +356,13 @@ function MapLegend() {
 
     legend.onAdd = function () {
       const div = L.DomUtil.create("div", "info legend");
+      const toggleButton = L.DomUtil.create("button", "toggleHider", div);
+      toggleButton.textContent = "collapse legend";
+      toggleButton.style.padding = "2px";
+      toggleButton.style.margin = "0";
+      toggleButton.style.marginBottom = "8px";
+      toggleButton.style.background = "hsl(288, 100%, 45%)";
+      toggleButton.style.color = "white";
 
       // White background + padding + rounded corners
       div.style.background = "white";
@@ -363,6 +370,20 @@ function MapLegend() {
       div.style.borderRadius = "6px";
       div.style.boxShadow = "0 0 10px rgba(0,0,0,0.2)";
 
+      const subDiv = L.DomUtil.create("div", "", div);
+      subDiv.style.overflow = "hidden";
+
+      toggleButton.onclick = function() {
+	if (subDiv.style.maxHeight !== '0px') {
+	  subDiv.style.maxHeight = "0px";
+	  toggleButton.style.marginBottom = "0";
+	  toggleButton.textContent = "show legend";
+	} else {
+	  subDiv.style.maxHeight = "unset";
+	  toggleButton.style.marginBottom = "8px";
+	  toggleButton.textContent = "collapse legend";
+	}
+      }
       // Get the grades for the legend
       const grades = gradientMapPoints(GradientMap0);
 
@@ -371,17 +392,20 @@ function MapLegend() {
         const to = grades[i + 1];
         const color = gradientMapNearest(from, GradientMap0);
 
-        div.innerHTML += `
+        subDiv.innerHTML += `
+          <span style="margin: auto; height: 5px; display:flex;">
           <i style="
             background:${color};
             width:18px;
             height:18px;
             display:inline-block;
             margin-right:8px;
-            opacity:0.7;
+	    opacity:0.9;
+            border: 1.5px solid black;
           "></i>
-          ${from}${to ? `&ndash;${to}` : "+"}<br/>
-        `;
+<b>${from}${to ? `&ndash;${to}` : "+"}</b>
+</span><br/>
+	`;
       }
 
       return div;
