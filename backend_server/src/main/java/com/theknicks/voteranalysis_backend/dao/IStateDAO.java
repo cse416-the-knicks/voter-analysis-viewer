ea4c3e5ed05ac85a6792dbad5b6828fd4fe866e8
Jerry Zhu <jerry.zhu@stonybrook.edu>
2025-10-01 21:55:56 -0400

    Starting to get work on the EAVs data endpoints, here's the first one... partially
    Backend and modern enterprise webcode is very obligatory and has lots
    of cruft, for the sake of quality.
    
    Which makes sense, but makes this take a long time.

--- backend_server/src/main/java/com/theknicks/voteranalysis_backend/dao/IStateDAO.java
+++ backend_server/src/main/java/com/theknicks/voteranalysis_backend/dao/IStateDAO.java
@@ -1,6 +1,8 @@
 package com.theknicks.voteranalysis_backend.dao;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.theknicks.voteranalysis_backend.models.ProvisionalBallotStatisticsModel;
+
 import java.util.*;
 
 /**
@@ -9,22 +11,27 @@ import java.util.*;
 public interface IStateDAO {
     /**
      * This access point is meant to return the geometry boundary of a
-     * state, along with some extra helper information to convey our use-case
-     * for this state. Primarily information on what type of detail state it is.
-     *
-     * EX:
-     * {
-     *     detailType: VOTER_REGISTRATION,
-     *     geoJson: ...
-     * }
-     *
-     * {
-     *     detailType: null, // or omitted.
-     *     geoJson: ...
-     * }
+     * state.
      *
      * @param fipsCode - A string for the fipsCode of the state.
      * @return GeoJSON object representing the boundaries of the state
      */
     Optional<ObjectNode> getGeometryBoundary(String fipsCode);
+
+    /**
+     * This access point is meant to return the provisional ballots for all the
+     * counties within a state.
+     * @param fipsCode - A string for the fipsCode of the state.
+     * @return a list of ProvisionalBallotStatisticsModels
+     */
+    List<ProvisionalBallotStatisticsModel> getProvisionBallotRow(String fipsCode);
+
+    /**
+     * This access point is meant to return the provisional ballots data for a specific county
+     * within a state.
+     * @param fipsCode - A string for the fipsCode of the state.
+     * @param countyCode - A string for the fipsCode of the county.
+     * @return a ProvisionalBallotStatisticModel if it exists, none otherwise.
+     */
+    Optional<ProvisionalBallotStatisticsModel> getProvisionBallotRowByCounty(String fipsCode, String countyCode);
 }
--- backend_server/src/main/java/com/theknicks/voteranalysis_backend/models/ProvisionalBallotStatisticsModel.java
+++ backend_server/src/main/java/com/theknicks/voteranalysis_backend/models/ProvisionalBallotStatisticsModel.java
@@ -1,4 +1,22 @@
 package com.theknicks.voteranalysis_backend.models;
 
-public record ProvisionalBallotStatistics() {
+/**
+ * This is the data necessary to cover the Provisional Ballots GUI
+ * use-cases.
+ */
+public record ProvisionalBallotStatisticsModel(
+        String fullRegionId,
+        String countyName,
+        int totalBallotsCast,
+        int ballotReasonNotOnList,
+        int ballotReasonNoIdAvailable,
+        int ballotReasonChallengedByOfficial,
+        int ballotReasonChallengedByOther,
+        int ballotReasonWrongPrecinct,
+        int ballotReasonNotUpdatedAddress,
+        int ballotReasonDidNotSurrender,
+        int ballotReasonExtendedVotingHours,
+        int ballotReasonSameDayRegistration,
+        int ballotReasonOther
+) {
 }
