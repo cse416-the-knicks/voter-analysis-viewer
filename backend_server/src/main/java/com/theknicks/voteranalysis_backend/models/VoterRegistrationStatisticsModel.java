b9b7842c6fa7da54531ae1d12b59665f27b9dad1
Jerry Zhu <jerry.zhu@stonybrook.edu>
2025-10-02 00:28:32 -0400

    Created some custom annotations to make some sql queries nearly automatic.

--- backend_server/src/main/java/com/theknicks/voteranalysis_backend/models/VoterRegistrationStatisticsModel.java
+++ backend_server/src/main/java/com/theknicks/voteranalysis_backend/models/VoterRegistrationStatisticsModel.java
@@ -1,15 +1,25 @@
 package com.theknicks.voteranalysis_backend.models;
 
+import com.theknicks.voteranalysis_backend.annotations.AutoSql;
+import com.theknicks.voteranalysis_backend.annotations.SqlColumnName;
+import com.theknicks.voteranalysis_backend.helpers.AutoSqlQueryable;
+
 /**
  * This is the data required to fulfill use-case 7,
  * the voter registration count.
  * A1a/b/c
  */
+@AutoSql(collection = "app.eavs_data")
 public record VoterRegistrationStatisticsModel(
-        String fullRegionId,
+        @SqlColumnName(name="region_id", omitFromAggregate=true) String fullRegionId,
         String countyName,
-        int total,
-        int active,
-        int inactive
+        @SqlColumnName(name="total_registered") int total,
+        @SqlColumnName(name="active_registered") int active,
+        @SqlColumnName(name="inactive_registered") int inactive
 ) {
+    public static class Queryable extends AutoSqlQueryable<VoterRegistrationStatisticsModel> {
+        public Queryable() {
+            super(VoterRegistrationStatisticsModel.class);
+        }
+    }
 }
