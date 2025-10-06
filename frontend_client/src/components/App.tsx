76c30db190884312242289cf5fc745b40ed99032
rajvirg26 <rajvir.ghumman@stonybrook.edu>
2025-10-06 12:17:18 -0400

    Experimental Theme Change

--- frontend_client/src/components/App.tsx
+++ frontend_client/src/components/App.tsx
@@ -13,13 +13,28 @@ import FrontPage from './FrontPage';
 import PartyComparisonView from './PartyComparisonView';
 import EarlyVotingComparisonView from './EarlyVotingComparisonView';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 interface DisplayPathOverlay {
   matchPortion: string,
   component: React.ReactNode;
 };
 
+const theme = createTheme({ 
+  colorSchemes: {
+  }
+});
+
+const darkTheme = createTheme({ 
+  colorSchemes: {
+    dark: true,
+  }
+});
+
 function App() {
   const location = useLocation();
+  const useDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   const showBlocker = location.pathname !== "/";
   const overlayPaths: DisplayPathOverlay[] = [
     { matchPortion: "/display/voting-machine-summary", component: <DisplayVotingMachineSummaryView/> },
@@ -31,17 +46,19 @@ function App() {
 
   return (
     <React.Fragment>
-      <Routes>
-        <Route path="/" element={<FrontPage/>}/>
-        {overlayPaths.map((x) => <Route path={x.matchPortion} element={<FrontPage/>}/>)}
-        <Route path="*" element={<NotFoundPage/>}/>
-      </Routes>
-      <BackgroundBlur showBlocker={showBlocker}/>
-      {/* NOTE(jerry): Needed in order to do the overlay effect that I think looks cool. */}
-      <Routes>
-        {overlayPaths.map((x) => <Route path={x.matchPortion} element={x.component}/>)}
-        <Route path="*" element={<React.Fragment/>}/>
-      </Routes> 
+      <ThemeProvider theme={(useDarkMode) ? darkTheme : theme}>
+        <Routes>
+          <Route path="/" element={<FrontPage/>}/>
+          {overlayPaths.map((x) => <Route path={x.matchPortion} element={<FrontPage/>}/>)}
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+        <BackgroundBlur showBlocker={showBlocker}/>
+        {/* NOTE(jerry): Needed in order to do the overlay effect that I think looks cool. */}
+        <Routes>
+          {overlayPaths.map((x) => <Route path={x.matchPortion} element={x.component}/>)}
+          <Route path="*" element={<React.Fragment/>}/>
+        </Routes> 
+      </ThemeProvider>
    </React.Fragment>
   )
 }
