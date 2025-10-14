4aa7cec6e5ba44eb9ca8de59823328027cb24822
{bengee42} <{bengee362@gmail.com}>
2025-10-13 21:19:52 -0400

    2020 EAVS data loader script

--- construct-db.sh
+++ construct-db.sh
@@ -58,5 +58,7 @@ pip install openpyxl # ???
 python3 load_prelim_states_data.py
 python3 load_boundary_data.py
 python3 load_2024_eavs_data.py
+python3 load_2022_eavs_data.py
+python3 load_2020_eavs_data.py
 
 touch  /var/lib/postgresql/.lockfile
