685f75292d57ead52ea9e4dd8ca2ead09e9c87d5
Jerry Zhu <jerry.zhu@stonybrook.edu>
2025-10-17 22:03:06 -0400

    Refactored / cleaned up DataGrid helpers. Supports loading on it's own.

--- frontend_client/src/components/StyledDataGrid/index.tsx
+++ frontend_client/src/components/StyledDataGrid/index.tsx
@@ -1,12 +1,22 @@
-import type { GridColDef, GridRowClassNameParams, GridRowIdGetter, GridValidRowModel } from '@mui/x-data-grid';
+import type {
+  GridColDef,
+  GridRowClassNameParams,
+  GridRowIdGetter,
+  GridValidRowModel,
+} from '@mui/x-data-grid';
 import { DataGrid } from '@mui/x-data-grid';
 import { Box } from '@mui/material';
+import {
+  useState,
+  useEffect,
+} from 'react';
 
 type getRowClassNameFn = (r: GridRowClassNameParams<GridValidRowModel>) => string;
+type RowMaker = readonly object[] | (() => Promise<object[]>);
 interface StyledDataGridProperties {
-  rows: readonly any[];
+  rows: RowMaker;
   columns: readonly GridColDef[];
-  getRowId: GridRowIdGetter;
+  getRowId?: GridRowIdGetter;
   pageSize: number;
   customGetRowClassName?: getRowClassNameFn;
   width?: number | string;
@@ -33,6 +43,28 @@ function StyledDataGrid(
       (r) => (r.indexRelativeToCurrentPage % 2) == 0 ? "oddRowStyle" : "";
     return (customGetRowClassName && customGetRowClassName(r)) + " " + colorAsAlternatingRows(r);
   }
+
+  // TODO(jerry): handle loading states. I don't care
+  // enough about this to do it here though, I think it's fine to
+  // pop-in if I'm being honest...
+  const [actualRows, setActualRows] = useState<readonly object[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(
+    function () {
+      if (typeof rows === "function") {
+	(async function() {
+	  const actualData = await rows();
+	  setActualRows(actualData);
+	  setIsLoaded(true);
+	})();
+      } else {
+	setActualRows(rows);
+	setIsLoaded(true);
+      }
+    }
+    ,[rows]);
+  
   return (
     <Box
       width={width}
@@ -40,7 +72,7 @@ function StyledDataGrid(
       maxWidth={maxWidth}
       maxHeight={maxHeight}>
       <DataGrid
-        rows={rows}
+        rows={actualRows}
         columns={columns}
         getRowId={getRowId}
         getRowClassName={getRowClassNameFunction}
@@ -67,3 +99,7 @@ function StyledDataGrid(
 }
 
 export default StyledDataGrid;
+export type {
+    RowMaker,
+    StyledDataGridProperties
+};
