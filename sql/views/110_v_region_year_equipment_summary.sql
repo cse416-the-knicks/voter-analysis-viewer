3b0dba998b6cf3e69603e929adb1d02f8e901ddf
s3anzi3 <s3anzie@gmail.com>
2025-11-17 16:41:14 -0500

    Updated Schema alongside erd

--- sql/views/110_v_region_year_equipment_summary.sql
+++ sql/views/110_v_region_year_equipment_summary.sql
@@ -1,7 +1,17 @@
-CREATE OR REPLACE VIEW app.v_region_year_equipment_summary AS
+CREATE OR REPLACE VIEW app.v_region_year_equipment_lite AS
 SELECT
-  eu.region_id,
-  eu.year,
-  SUM(eu.quantity) AS total_devices
+    eu.region_id,
+    eu.state_id,
+    s.code AS state_code,
+    s.name AS state_name,
+    eu.year,
+    eu.device_model_id,
+    dm.manufacturer,
+    dm.model_name,
+    dm.equipment_type,
+    eu.quantity
 FROM app.equipment_usage eu
-GROUP BY eu.region_id, eu.year;
+JOIN app.states s
+  ON s.state_id = eu.state_id
+JOIN app.device_model dm
+  ON dm.device_model_id = eu.device_model_id;
--- sql/views/111_v_device_model_usage.sql
+++ sql/views/111_v_device_model_usage.sql
@@ -1,14 +1,38 @@
 CREATE OR REPLACE VIEW app.v_device_model_usage AS
 SELECT
-  dm.device_model_id,
-  dm.vendor,
-  dm.model_name,
-  dm.device_type,
-  dm.year_introduced,
-  dm.certification,
-  COUNT(DISTINCT eu.region_id) AS regions_using,
-  SUM(eu.quantity)             AS units_deployed
+    dm.device_model_id,
+    dm.manufacturer,
+    dm.model_name,
+    dm.equipment_type,
+    dm.first_manufactured,
+    dm.last_manufactured,
+    dm.certification_level AS certification,
+    dm.os,
+    dm.battery_life,
+    dm.scanning_rate,
+    dm.paper_capacity,
+    dm.vvpatt,
+    dm.error_rate,
+    dm.reliability,
+    dm.quality_score,
+    COUNT(DISTINCT eu.region_id) AS regions_using,
+    COALESCE(SUM(eu.quantity), 0) AS units_deployed
 FROM app.device_model dm
-LEFT JOIN app.equipment_usage eu ON eu.device_model_id = dm.device_model_id
-GROUP BY dm.device_model_id, dm.vendor, dm.model_name, dm.device_type, dm.year_introduced, dm.certification;
-
+LEFT JOIN app.equipment_usage eu
+       ON eu.device_model_id = dm.device_model_id
+GROUP BY
+    dm.device_model_id,
+    dm.manufacturer,
+    dm.model_name,
+    dm.equipment_type,
+    dm.first_manufactured,
+    dm.last_manufactured,
+    dm.certification_level,
+    dm.os,
+    dm.battery_life,
+    dm.scanning_rate,
+    dm.paper_capacity,
+    dm.vvpatt,
+    dm.error_rate,
+    dm.reliability,
+    dm.quality_score;
