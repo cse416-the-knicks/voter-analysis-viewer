90f7ab66174d26b60743baf4425a3f0ab66722b5
Jerry Zhu <jerry.zhu@stonybrook.edu>
2025-11-21 14:56:26 -0500

    Updated and tested implementation, looks not broken

--- backend_server/src/main/java/com/theknicks/voteranalysis_backend/models/GeoUnitCentroidDataRowModel.java
+++ backend_server/src/main/java/com/theknicks/voteranalysis_backend/models/GeoUnitCentroidDataRowModel.java
@@ -5,25 +5,22 @@ import com.theknicks.voteranalysis_backend.annotations.SqlColumnName;
 import com.theknicks.voteranalysis_backend.helpers.AutoSqlQueryable;
 
 /**
- * Similar to GeoUnitCentroidModel, but this is the raw copy that is
- * retrieved from the database.
+ * Similar to GeoUnitCentroidModel, but this is the raw copy that is retrieved from the database.
  *
- * Needs to be converted into the regular GeoUnitCentroidModel
+ * <p>Needs to be converted into the regular GeoUnitCentroidModel
  */
 @AutoSql(
-        collection = "app.region_boundary",
-        joining = {"app.eavs_geounit"},
-        joinMethod = {"inner"},
-        joinOn = {"eavs_geounit.eavs_unit_code = region_boundary.region_id"}
-)
+    collection = "app.region_boundary",
+    joining = {"app.eavs_geounit"},
+    joinMethod = {"inner"},
+    joinOn = {"eavs_geounit.eavs_unit_code = region_boundary.region_id"})
 public record GeoUnitCentroidDataRowModel(
-        @SqlColumnName(name="region_boundary.region_id") String regionId,
-        @SqlColumnName(name="eavs_geounit.name") String name,
-        @SqlColumnName(name="geom_center") String geomCenterJson
-) {
-    public static class Queryable extends AutoSqlQueryable<GeoUnitCentroidDataRowModel> {
-        public Queryable() {
-            super(GeoUnitCentroidDataRowModel.class);
-        }
+    @SqlColumnName(name = "region_boundary.region_id") String regionId,
+    @SqlColumnName(name = "eavs_geounit.name") String name,
+    @SqlColumnName(name = "geom_center") String geomCenterJson) {
+  public static class Queryable extends AutoSqlQueryable<GeoUnitCentroidDataRowModel> {
+    public Queryable() {
+      super(GeoUnitCentroidDataRowModel.class);
     }
+  }
 }
