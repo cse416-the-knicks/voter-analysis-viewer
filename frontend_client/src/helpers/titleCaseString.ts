80b1a150ea415f1731798b89687f03e6012ab314
jerzhu <143643544+jerzhu@users.noreply.github.com>
2025-12-12 14:08:52 -0500

    [BUGBASH] GUI19 affiliated selections, more voter registration info (#180)
    * buncha stuff, more infos, affiliated
    
    * FORMAT

--- frontend_client/src/helpers/titleCaseString.ts
+++ frontend_client/src/helpers/titleCaseString.ts
@@ -1,9 +1,17 @@
+function titleCaseWord(word: string) {
+  if (word === null) {
+    return word;
+  }
+  const lowercased = word.toLowerCase();
+  return lowercased.charAt(0).toUpperCase() + lowercased.slice(1);
+}
+
 function titleCaseString(value: string) {
   if (value === null) {
     return value;
   }
-  const lowercased = value.toLowerCase();
-  return lowercased.charAt(0).toUpperCase() + lowercased.slice(1);
+  const split = value.split(" ");
+  return split.map(titleCaseWord).join(" ");
 }
 
 export default titleCaseString;
