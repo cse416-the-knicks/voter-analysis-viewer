a8fee1a2b47265bc68fd6f5571d7e1a096bea2a0
Jerry Zhu <jerry.zhu@stonybrook.edu>
2025-12-13 23:36:10 -0500

    final

--- frontend_client/src/components/GenericTooltip/index.tsx
+++ frontend_client/src/components/GenericTooltip/index.tsx
@@ -18,14 +18,15 @@ function GenericTooltip({ children, show }: GenericTooltipProperties) {
             position: "fixed",
             left: mousePosition.x + "px",
             top: mousePosition.y + 16 + "px",
-            background: "rgba(0.8,0.8,0.8,0.7)",
+            backgroundColor: "rgba(0.9,0.8,0.2,0.7)",
             borderRadius: "8px",
+            backdropFilter: "blur(1px)", 
             padding: "6px",
             pointerEvents: "none",
             color: "white",
             zIndex: 9999,
           }}
-          elevation={3}
+          elevation={5}
         >
           {children}
         </Paper>
